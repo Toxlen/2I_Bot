65ff798ab1022dc0b97466e31b554b117fc5e18d
CHAMPSEIX Thomas <thomas.champseix@etu.univ-lyon1.fr>
2020-11-14 20:37:26 +0100

    Ajout des devoirs dans le fichier des devoirs et ajout de __pycache__ dans les choses à ignorer

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
-import typing
+import json
+# import typing # C'est pour si on veut faire des argument non obligatoire et pour le moment c'est plus le cas
 from datetime import datetime
 
 from discord.ext import commands
@@ -20,13 +21,23 @@ async def ping(ctx):
 
 @bot.command(name='add', help="Ajoute des devoirs dans la liste de devoirs")
 async def add(ctx, date: str, matiere: str, *, description):
+    
     date = dateFormating(date)
 
     if date == None:
         raise commands.ConversionError
 
-    
+    devoirs = ""
 
+    with open("devoirs.json", "r") as myfile:
+        devoirs = json.load(myfile)
+
+    with open("devoirs.json", "w") as myfile:
+        toAppend = {"name" : matiere, "value" : description, "date" : date.isoformat()}
+        devoirs["fields"].append(toAppend)
+        print(devoirs)
+        myfile.write(json.dumps(devoirs))
+    
     date_str = date.strftime("%d/%m/%Y")
     response = f"J'ajoute le devoir de {matiere} pour le {date_str} vous serez prevenue la veille ! \nVous pouvez également voir la liste des devoirs en tapant !devoirs"
     await ctx.send(response)

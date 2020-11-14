8edbf0490ccb0c19736145a70c7772323b08421e
CHAMPSEIX Thomas <thomas.champseix@etu.univ-lyon1.fr>
2020-11-14 18:05:49 +0100

    Capture dans le bon format

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
 import typing
-from datetime import date
+from datetime import datetime
 
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -17,10 +17,36 @@ async def ping(ctx):
     await ctx.send(response)
 
 @bot.command(name='add', help="Ajoute des devoirs dans la liste de devoirs")
-async def add(ctx, jour: int, mois: int, annee: typing.Optional[int] = date.today().year):
-    date_devoir = date(annee, mois, jour)
-    date_devoir_str = date_devoir.strftime("%d/%m/%y")
+async def add(ctx, date: str):
+    if isFormat(date,"%d/%m/%y"):
+        date_devoir = datetime.strptime(date,"%d/%m/%y")
+    elif isFormat(date,"%d-%m-%y"):
+        date_devoir = datetime.strptime(date,"%d-%m-%y")
+    elif isFormat(date,"%d/%m"):
+        date_devoir = datetime.strptime(date,"%d/%m")
+        date_devoir.replace(year=datetime.now().year)
+    elif isFormat(date,"%d-%m"):
+        date_devoir = datetime.strptime(date,"%d-%m")
+        date_devoir.replace(year=datetime.now().year)
+    else :
+        raise commands.BadArgument
+
+    date_devoir_str = date_devoir.strftime("%d/%m/%Y")
     response = f"J'ajoute le devoir {date_devoir_str}"
     await ctx.send(response)
+
+@add.error
+async def add_error(ctx, error):
+    if isinstance(error, commands.BadArgument) :
+        await ctx.send("Fait attention au format de la date !")
+
+def isFormat(date, format):
+    try:
+        if date != datetime.strptime(date, format).strftime(format):
+            raise ValueError
+        return True
+    except ValueError:
+        return False
+
 
 bot.run(TOKEN)

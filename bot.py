5cd11f6302aab071a5be80e8dc47e95038276485
CHAMPSEIX Thomas <thomas.champseix@etu.univ-lyon1.fr>
2020-11-14 15:55:38 +0100

    Toujours plus de propreté

--- bot.py
+++ bot.py
@@ -7,14 +7,13 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+class CustomClient(discord.Client):
+    async def on_ready(self):
+        guild = discord.utils.get(client.guilds, name=GUILD)
+        print(
+            f'{client.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})'
+        )
 
-@client.event
-async def on_ready():
-    guild = discord.utils.get(client.guilds, name=GUILD)
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
-
+client = CustomClient()
 client.run(TOKEN)

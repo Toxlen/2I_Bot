0026e58f7849a936574ee788013bc0abf64fcbbf
CHAMPSEIX Thomas <thomas.champseix@etu.univ-lyon1.fr>
2020-11-14 21:21:01 +0100

    affichage des devoirs dans le chat dans 2 formats différents (le tris des données n'est pas encore fait)

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import json
-# import typing # C'est pour si on veut faire des argument non obligatoire et pour le moment c'est plus le cas
+import discord
+import typing # C'est pour si on veut faire des argument non obligatoire
 from datetime import datetime
 
 from discord.ext import commands
@@ -14,11 +15,13 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix='!')
 
+# Test du ping pong
 @bot.command(name='ping', help="Répond pong")
 async def ping(ctx):
     response = "pong"
     await ctx.send(response)
 
+# Ajouter les devoirs dans le .json des devoirs
 @bot.command(name='add', help="Ajoute des devoirs dans la liste de devoirs")
 async def add(ctx, date: str, matiere: str, *, description):
     
@@ -35,17 +38,54 @@ async def add(ctx, date: str, matiere: str, *, description):
     with open("devoirs.json", "w") as myfile:
         toAppend = {"name" : matiere, "value" : description, "date" : date.isoformat()}
         devoirs["fields"].append(toAppend)
-        print(devoirs)
         myfile.write(json.dumps(devoirs))
     
     date_str = date.strftime("%d/%m/%Y")
     response = f"J'ajoute le devoir de {matiere} pour le {date_str} vous serez prevenue la veille ! \nVous pouvez également voir la liste des devoirs en tapant !devoirs"
     await ctx.send(response)
-
+# Gestion des erreur de la commande add
 @add.error
 async def add_error(ctx, error):
     if isinstance(error, commands.ConversionError) :
         await ctx.send("Fait attention au format de la date !")
+
+# Afficher les devoirs à faire
+@bot.command(help="Permet d'afficher les devoirs à faire")
+async def devoirs(ctx, parameter: typing.Optional[str] = "-m", *, description: typing.Optional[str] = "" ):
+    devoirs = ""
+    with open("devoirs.json", "r") as myfile:
+        devoirs = json.load(myfile)
+
+    if parameter == "-m":
+        if description != "":
+            pass
+        else:
+            i = 0
+            for element in devoirs["fields"]:
+                date = datetime.fromisoformat(element["date"])
+                date_str = date.strftime("%d/%m/%Y")
+                devoirs["fields"][i]["value"] += " pour le " + date_str
+                i += 1
+            
+    elif parameter == "-d":
+        i = 0
+        for element in devoirs["fields"]:
+            date = datetime.fromisoformat(element["date"])
+            date_str = date.strftime("%d/%m/%Y")
+            devoirs["fields"][i]["name"] = date_str + " : " + devoirs["fields"][i]["name"]
+            i += 1
+    else:
+        raise commands.BadArgument
+        
+
+    miseEnForme = discord.Embed.from_dict(devoirs)
+    await ctx.send(embed= miseEnForme)
+# Gestion des erreur de la commande devoirs
+@devoirs.error
+async def devoirs_error(ctx, error):
+    if isinstance(error, commands.BadArgument) :
+        await ctx.send("Tu as mal écris la commande !")
+
 
 
 bot.run(TOKEN)
